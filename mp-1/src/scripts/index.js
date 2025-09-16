2ca7989682dea5dfbecd344af084e5f00eaebb13
Isaac Chan <isaacch@bu.edu>
2025-09-16 16:18:00 -0400

    added projects section

--- mp-1/src/scripts/index.js
+++ mp-1/src/scripts/index.js
@@ -3,5 +3,34 @@ let input2 = document.getElementById("input2");
 let output = document.getElementById("output");
 
 function doAdd() {
+  output.style.color = "#f5e9dc";
   output.innerHTML = String(Number(input1.value) + Number(input2.value));
+}
+
+function doSubtract() {
+  output.innerHTML = String(Number(input1.value) - Number(input2.value));
+  if (Number(output.innerHTML) < 0) {
+    output.style.color = "red";
+  }
+}
+
+function doMultiply() {
+  output.style.color = "#f5e9dc";
+  output.innerHTML = String(Number(input1.value) * Number(input2.value));
+}
+
+function doDivide() {
+  output.style.color = "#f5e9dc";
+  output.innerHTML = String(Number(input1.value) / Number(input2.value));
+}
+
+function doPower() {
+  output.style.color = "#f5e9dc";
+  output.innerHTML = String(Number(input1.value) ** Number(input2.value));
+}
+
+function doClear() {
+  input1.value = "";
+  input2.value = "";
+  output.innerHTML = "0";
 }
